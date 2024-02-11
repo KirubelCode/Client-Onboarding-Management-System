cc2b4def859ff6b1555e7c3959aa44e6aa4e83f3
KirubelCode <c00260396@setu.ie>
2024-02-11 16:00:14 +0000

    Ui Tidying and automatic page reload

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 function generateLink() {
-    
-
-    // Get  selected platform (e.g., Instagram, TikTok, Facebook)
     var selectedPlatform = document.querySelector('input[name="platform"]:checked');
 
     if (selectedPlatform) {
         var platformValue = selectedPlatform.value;
-        var generatedLink = ""; // Generate the link based on the selected platform
+        var generatedLink = "";
 
-        
         if (platformValue === "instagram") {
             generatedLink = "https://www.instagram.com/";
         } else if (platformValue === "tiktok") {
@@ -17,14 +13,49 @@ function generateLink() {
             generatedLink = "https://www.facebook.com/";
         }
 
-        // Display the generated link in the 'generated-link' div
-        var generatedLinkDiv = document.querySelector('.generated-link');
-        generatedLinkDiv.textContent = generatedLink;
+        sessionStorage.setItem('selectedLink', generatedLink); // Store selected link in session
+
+        window.location.href = "checkPermissions.html"; // Redirect to checkPermissions.html
     } else {
-        alert("Please select a platform."); // notify user if no platform is selected
+        alert("Please select a platform.");
     }
 }
 
+
+
 function goBack() {
-    window.history.back(); // Go back to the previous page
+    window.history.back(); // Previous page
+}
+
+function displayLink() {
+    const linkContainer = document.getElementById('linkContainer');
+    const link = sessionStorage.getItem('selectedLink'); // Retrieve selected link from session storage
+
+    if (link) {
+        linkContainer.textContent = link;
+    } else {
+        linkContainer.textContent = 'No link selected.';
+    }
+    
+}
+
+window.onload = () => {
+    displayLink();
+    setInterval(() => {
+        window.location.reload();
+    }, 10000); // Reload the page every 10 seconds
+}
+
+function copyLink() {
+    const link = sessionStorage.getItem('selectedLink');
+
+    if (link) {
+        navigator.clipboard.writeText(link) // Copy to clipoard using the api
+            .then(() => {
+                alert('Link copied to clipboard: ' + link);
+            })
+            
+    } else {
+        alert('No link to copy.');
+    }
 }
