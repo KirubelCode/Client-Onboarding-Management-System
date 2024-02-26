fd79f7b336a2e95f59f317ac965a8b1b3fc05c43
KirubelCode <145583088+KirubelCode@users.noreply.github.com>
2024-02-26 17:21:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/index.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.js'));
 });
 
+app.get('/onboard.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'onboard.html'));
+});
+
 // Define route to serve onboard.html when accessing the root path
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
