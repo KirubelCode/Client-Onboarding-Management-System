a9180e83a5fbc5860c625b5a9b124c28f94489b8
KirubelCode <145583088+KirubelCode@users.noreply.github.com>
2024-02-26 16:50:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path'); // Import the path module
 const PORT = process.env.PORT || 3000;
-
-// Import route handlers
-const authRoutes = require('./routes/authRoutes');
-const clientRoutes = require('./routes/clientRoutes');
 
 // Middleware to serve static files (HTML, CSS, JS) from the public directory
 app.use(express.static('public'));
 
-// Define routes
-app.use('/auth', authRoutes); // Routes related to authentication
-app.use('/client', clientRoutes); // Routes related to clients
+// Define route to serve onboard.html when accessing the root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'onboard.html'));
+});
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+
+
 
