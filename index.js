4bc7112e0f5215d54ea7df7b8499367d0e63ff10
KirubelCode <c00260396@setu.ie>
2024-02-29 11:37:58 +0000

    Checking user consent for data usage

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
