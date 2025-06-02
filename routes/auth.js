7185f3775cbd1d924815a10e87146510831479b0
KirubelCode <c00260396@setu.ie>
2025-06-02 16:17:55 +0100

    All updated Ui for simplicity and a common theme

--- routes/auth.js
+++ routes/auth.js
@@ -188,4 +188,13 @@ router.post("/login", async (req, res) => {
     }
 });
 
+router.post("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send("Logout failed");
+    }
+    res.redirect("/login");
+  });
+});
+
 module.exports = router;
