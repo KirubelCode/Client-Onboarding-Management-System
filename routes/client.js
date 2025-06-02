9d39b000947ab588656b658e77c79e5f48ffb06c
KirubelCode <c00260396@setu.ie>
2025-06-02 17:38:40 +0100

    Adjusting minor ui issues

--- routes/client.js
+++ routes/client.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const mysql = require("mysql2");
 const router = express.Router();
 
@@ -148,5 +149,101 @@ router.post('/deleteClient', (req, res) => {
     });
 });
 
+// GET: Manage Clients Page
+
+// GET: Manage Clients Page
+router.get("/manage-clients", async (req, res) => {
+  const userDbConfig = req.session.userDbConfig;
+  if (!userDbConfig) return res.status(401).send("Not authenticated");
+
+  const conn = establishUserDbConnection(userDbConfig);
+  conn.connect();
+
+  conn.query("SELECT * FROM clientInfo", (err, results) => {
+    if (err) {
+      conn.end();
+      return res.status(500).send("DB error");
+    }
+
+    const clientOptions = results.map(c => `<option value="${c.ID}">${c.firstName} ${c.lastName}</option>`).join('');
+    const clientDetails = JSON.stringify(
+      results.reduce((obj, c) => {
+        obj[c.ID] = c;
+        return obj;
+      }, {})
+    );
+
+    fs.readFile(path.join(__dirname, "../views/manage-clients.html"), "utf8", (err, html) => {
+      if (err) {
+        conn.end();
+        return res.status(500).send("Could not load HTML");
+      }
+
+      html = html.replace("<!-- Insert ${clientOptions} here from server -->", clientOptions);
+      html = html.replace("const clientDetails = {};", `const clientDetails = ${clientDetails};`);
+
+      res.send(html);
+      conn.end();
+    });
+  });
+});
+
+// GET: Search Clients Page
+router.get("/searchClientsPage", (req, res) => {
+  fs.readFile(path.join(__dirname, "../views/search-clients.html"), "utf8", (err, html) => {
+    if (err) return res.status(500).send("Could not load search page");
+    res.send(html);
+  });
+});
+
+// POST: Search Logic
+router.post("/searchClientAddress", (req, res) => {
+  const { keyword } = req.body;
+  const config = req.session.userDbConfig;
+  if (!config) return res.status(401).send("Unauthorized");
+
+  const conn = establishUserDbConnection(config);
+  conn.connect();
+
+  conn.query("SELECT * FROM clientInfo WHERE address LIKE ?", [`%${keyword}%`], (err, rows) => {
+    conn.end();
+    if (err) return res.status(500).send("DB Error");
+
+    if (rows.length === 0) return res.send("<p>No clients found.</p>");
+
+    const html = rows.map(c => `
+      <div>
+        <b>${c.firstName} ${c.lastName}</b><br>
+        ${c.email}<br>
+        ${c.phone}<br>
+        ${c.address}
+        <hr>
+      </div>
+    `).join("");
+
+    res.send(html);
+  });
+});
+
+
+// POST: Search Logic
+router.post("/searchClientAddress", (req, res) => {
+    const { keyword } = req.body;
+    const config = req.session.userDbConfig;
+    if (!config) return res.status(401).send("Unauthorized");
+
+    const conn = establishUserDbConnection(config);
+    conn.connect();
+    conn.query("SELECT * FROM clientInfo WHERE address LIKE ?", [`%${keyword}%`], (err, rows) => {
+        if (err) return res.status(500).send("DB Error");
+
+        let html = "";
+        rows.forEach(c => {
+            html += `<div><b>${c.firstName} ${c.lastName}</b><br>${c.email}<br>${c.phone}<br>${c.address}<hr></div>`;
+        });
+        res.send(html || "<p>No clients found.</p>");
+        conn.end();
+    });
+});
 
 module.exports = router;
